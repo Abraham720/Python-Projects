48b5634d132bcd1efe9783cf983fbeeb24811209
Unknown140 <167537619+Abraham720@users.noreply.github.com>
2024-12-23 12:31:34 +0700

    Add files via upload

---
+++ Number guess.py
@@ -0,0 +1,53 @@
+from random import *
+print('\nWelcome to \nNumber Guessing game!\n')
+
+again = ''
+
+
+while again != 'no':
+
+    min = int(input("What's the minimum number? "))
+    max = int(input("What's the maximum number? "))
+    guesses = 1
+    rating = ''
+
+    number = randint(min, max)
+    guess = int(input("\nA random number has been made.\nWhat's your guess? "))
+
+    while guess != number:
+
+        if guess > number:
+            print('\nYou guess too high! Try lower ')
+            guess = int(input("\nWhat's your next guess? "))
+            guesses += 1
+
+        elif guess < number:
+            print('\nYou guess too low! Try higher')
+            guess = int(input("\nWhat's your next guess? "))
+            guesses += 1
+
+
+    if guesses == 1 or guesses == 2:
+        rating = 'Imposible!'
+
+    elif guesses < 5:
+        rating = 'Outstanding!'
+
+    elif guesses < 7:
+        rating = 'Good'
+        
+    elif guesses < 9:
+        rating = 'Ok'
+
+    elif guesses < 11:
+        rating = 'Meh'
+
+    else:
+        rating = 'Bad try'
+
+
+    print('\nYou are correct! the number was', number ,'\nYou guess the number within ' , guesses , 'guesses/try!' , rating)
+
+    again = input('\nDo you want to play again? ')
+
+print("\nThank you for playing! have a nice day :D")
